5e2332a8e4b1dda384434b06159a00886a0e03d0
zaksorel <sorelzakary@gmail.com>
2023-05-17 16:20:59 -0400

    delete unused files

--- lib/screens/social_hub.dart
+++ lib/screens/social_hub.dart
@@ -9,12 +9,30 @@ class SocialHub extends StatelessWidget {
     return PageView(
       scrollDirection: Axis.horizontal,
       controller: _pageController,
-      children: const [
+      children: [
         //messages
+        Container(
+          color: Colors.red,
+          child: const Center(
+              child: Text('messages',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40))),
+        ),
 
         //chatbox
+        Container(
+          color: Colors.green,
+          child: const Center(
+              child: Text('chatbox',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40))),
+        ),
 
         //friends
+        Container(
+          color: Colors.yellow,
+          child: const Center(
+              child: Text('friends',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40))),
+        ),
       ],
     );
   }
