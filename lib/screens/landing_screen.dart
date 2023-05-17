4c32980700da03f63d09ea59b05c1510d285b7f0
zaksorel <sorelzakary@gmail.com>
2023-05-16 23:38:07 -0400

    the whole chabang

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -2,20 +2,100 @@ import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import '../utils/logo.dart';
 
+//handle page view
+final _pageController = PageController(initialPage: 1);
+//click pages
+final pageView = PageView(
+  scrollDirection: Axis.horizontal,
+  controller: _pageController,
+  children: [
+    GridView.count(
+      crossAxisCount: 4,
+      // Generate 100 widgets that display their index in the List.
+      children: List.generate(100, (index) {
+        return Card(
+          color: Colors.blue,
+          child: Center(
+            child: Text(
+              'upgrade $index',
+            ),
+          ),
+        );
+      }),
+    ),
+    Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: FloatingActionButton.large(
+                  onPressed: () {
+                    print('click');
+                  },
+                  backgroundColor: Colors.green,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: FloatingActionButton.large(
+                  onPressed: () {
+                    print('click');
+                  },
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+    GridView.count(
+      crossAxisCount: 4,
+      // Generate 100 widgets that display their index in the List.
+      children: List.generate(100, (index) {
+        return Card(
+          color: Colors.yellow,
+          child: Center(
+            child: Text(
+              'upgrade $index',
+            ),
+          ),
+        );
+      }),
+    ),
+  ],
+);
+
 //list of pages for navigation bar
 final List<Widget> pageList = [
   Container(
-    alignment: Alignment.center,
-    child: const Text('home',
-        style:
-            TextStyle(color: white, fontWeight: FontWeight.bold, fontSize: 70)),
-  ),
-  Container(
-    alignment: Alignment.center,
-    child: const Text('click',
-        style:
-            TextStyle(color: white, fontWeight: FontWeight.bold, fontSize: 70)),
-  ),
+      alignment: Alignment.center,
+      child: GridView.count(
+        mainAxisSpacing: 0,
+        crossAxisSpacing: 0,
+        crossAxisCount: 3,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+          return Container(
+            color: white,
+            alignment: Alignment.center,
+            child: Center(
+              child: Text(
+                'Item $index',
+              ),
+            ),
+          );
+        }),
+      )),
+  pageView,
   Container(
     alignment: Alignment.center,
     child: const Text('profile',
@@ -38,6 +118,15 @@ class _LandingScreenState extends State<LandingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //top nav bar
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(25.0), // here the desired height
+        child: Container(
+          color: Colors.transparent,
+          child: const Center(child: Text('My Custom AppBar')),
+        ),
+      ),
+
       //main body
       body: pageList[currentPageIndex],
 
